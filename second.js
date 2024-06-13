55560e200bfc7c7a5673df84c64b2cc39a45cdba
MohammadShohelhan <shohel11999khan@gmail.com>
2024-06-13 13:46:01 +0600

    first commit

---
+++ second.js
@@ -0,0 +1,14 @@
+// const seeMore = document.querySelector("#seeMore");
+// const seeMoreItem = document.querySelector(".second-manu-container");
+// let see = 0;
+
+// seeMore.addEventListener("click", function() {
+//     if (see === 0) {
+//         seeMoreItem.classList.remove("second-manu-container");
+//         see = 1;
+//     } else {
+//         seeMoreItem.classList.add("second-manu-container");
+//         see = 0;
+//     }
+// });
+
